68aa00d1752a2439c3e0426b6c84d702f968332e
icomputercoding <vaibhavsonu0506@gmail.com>
2022-08-15 22:21:17 +0530

    Completed the task

--- app/src/main/java/in/icomputercoding/mycalculator/MainActivity.kt
+++ app/src/main/java/in/icomputercoding/mycalculator/MainActivity.kt
@@ -1,8 +1,10 @@
 package `in`.icomputercoding.mycalculator
 
 import `in`.icomputercoding.mycalculator.databinding.ActivityMainBinding
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,80 +24,83 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.BtnZero.setOnClickListener {
-            evaluateExpression("0",true)
+            evaluateExpression("0", true)
         }
 
         binding.BtnZero2.setOnClickListener {
-            evaluateExpression("00",true)
+            evaluateExpression("00", true)
         }
 
         binding.BtnOne.setOnClickListener {
-            evaluateExpression("1",true)
+            evaluateExpression("1", true)
         }
 
         binding.BtnTwo.setOnClickListener {
-            evaluateExpression("2",true)
+            evaluateExpression("2", true)
         }
 
         binding.BtnThree.setOnClickListener {
-            evaluateExpression("3",true)
+            evaluateExpression("3", true)
         }
 
         binding.BtnFour.setOnClickListener {
-            evaluateExpression("4",true)
+            evaluateExpression("4", true)
         }
 
         binding.BtnFive.setOnClickListener {
-            evaluateExpression("5",true)
+            evaluateExpression("5", true)
         }
 
         binding.BtnSix.setOnClickListener {
-            evaluateExpression("6",true)
+            evaluateExpression("6", true)
         }
 
         binding.BtnSeven.setOnClickListener {
-            evaluateExpression("7",true)
+            evaluateExpression("7", true)
         }
 
         binding.BtnEight.setOnClickListener {
-            evaluateExpression("8",true)
+            evaluateExpression("8", true)
         }
 
         binding.BtnNine.setOnClickListener {
-            evaluateExpression("9",true)
+            evaluateExpression("9", true)
         }
 
 
         binding.BtnDot.setOnClickListener {
-            evaluateExpression(".",true)
+            evaluateExpression(".", true)
         }
 
-        binding.BtnPlus.setOnClickListener{
-            evaluateExpression("+",true)
+        binding.BtnPlus.setOnClickListener {
+            evaluateExpression("+", false)
         }
 
-        binding.BtnMinus.setOnClickListener{
-            evaluateExpression("-",true)
+        binding.BtnMinus.setOnClickListener {
+            evaluateExpression("-", false)
         }
 
-        binding.BtnMultiply.setOnClickListener{
-            evaluateExpression("*", true)
+        binding.BtnMultiply.setOnClickListener {
+            evaluateExpression("*", false)
         }
 
         binding.BtnDivide.setOnClickListener {
-            evaluateExpression("/", true)
+            evaluateExpression("/", false)
         }
 
         binding.BtnAns.setOnClickListener {
-            val text = binding.Input.text.toString()
-            val expression = ExpressionBuilder(text).build()
 
-            val result = expression.evaluate()
-            val longResult = result.toLong()
-            if (result == longResult.toDouble()) {
-                binding.result.text = longResult.toString()
-            } else {
-                binding.result.text = result.toString()
+            try {
+                val expression = ExpressionBuilder(binding.Input.text.toString()).build()
+                val result = expression.evaluate()
+                val longResult = result.toLong()
+                if (result == longResult.toDouble()) {
+                    binding.result.text = longResult.toString()
+                } else
+                    binding.result.text = result.toString()
+
+            } catch (e: Exception) {
+                Toast.makeText(this,"Invalid Operation",Toast.LENGTH_SHORT).show()
             }
         }
 
@@ -103,7 +109,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun evaluateExpression(string: String, clear: Boolean) {
-        if(clear) {
+        if (clear) {
             binding.result.text = ""
             binding.Input.append(string)
         } else {
